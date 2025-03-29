b17a1187ee0f4cbfe109c85492622d31b956871a
Nam Hữu Nam <tranhuunam2000@gmail.com>
2025-03-30 01:26:55 +0700

    Update GeminiApiService.php

--- src/Services/GeminiApiService.php
+++ src/Services/GeminiApiService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace HuyUDB24CC149\MovieContentGenerator\Services;
+namespace namhuunam\MovieContentGenerator\Services;
 
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
