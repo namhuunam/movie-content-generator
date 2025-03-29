d3cbeb478c034c11f0d8134ea6056de877002e63
Nam Hữu Nam <tranhuunam2000@gmail.com>
2025-03-30 03:05:35 +0700

    Delete src directory

--- src/config/logging.php
+++
@@ -1,6 +0,0 @@
-// Add this to your existing config/logging.php channels array
-'movie-content' => [
-    'driver' => 'single',
-    'path' => storage_path('logs/movie-content.log'),
-    'level' => 'debug',
-],
